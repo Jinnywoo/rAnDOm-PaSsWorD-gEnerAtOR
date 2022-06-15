dfcf020617fa2b1f5417a7d940ad84a7e7d0a2c1
Ares <jinwoo2639@gmail.com>
2022-06-15 16:06:09 -0700

    adding prompts and variable storage for confirming the boolean value of password characters

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 var specialChar = ["!","@","#","$","%","^","&","*",];
 var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercaseChar = lowercaseChar.map(lowercaseChar => lowercaseChar.toUpperCase());
-console.log(lowercaseChar)
+
 
 function generatePassword(){
 
+  var passwordLength = prompt("How many Characters would you like your password to be?");
 
+  if (passwordLength >= 8 && passwordLength <=128){
+    
+  }else{
+   confirm("You must select a password length between 8 and 128 characters long.");
+  }
+  
+  var numberConfirm = confirm("Click OK to confirm including numeric characters");
+  var lowerConfirm = confirm("Click OK to confirm including lowercase alphabetical characters");
+  var upperConfirm = confirm("Click OK to confirm including uppercase alphabetical characters");
+  var specConfirm = confirm("Click OK to confirm including special characters");
+
+  console.log(numberConfirm);
+  console.log(lowerConfirm);
+  console.log(upperConfirm);
+  console.log(specConfirm);
+  
 }
 
 
