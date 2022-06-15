559370f98297ec7b94db6f7a328b758876c3a70c
Ares <jinwoo2639@gmail.com>
2022-06-15 15:38:50 -0700

    Added variable values for password content

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var specialChar = ["!","@","#","$","%","^","&","*",];
+var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseChar = lowercaseChar.map(lowercaseChar => lowercaseChar.toUpperCase());
+console.log(lowercaseChar)
+
+function generatePassword(){
+
+
+}
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
